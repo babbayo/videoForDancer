347e16731c912000753178a5e179577fb6d54624
babbayo <na.babbayo@gmail.com>
2020-01-08 21:33:20 +0900

    init

---
+++ videoForDancer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  videoForDancer
+//
+//  Created by USER on 2020/01/08.
+//  Copyright © 2020 erin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ videoForDancerTests/videoForDancerTests.swift
@@ -0,0 +1,34 @@
+//
+//  videoForDancerTests.swift
+//  videoForDancerTests
+//
+//  Created by USER on 2020/01/08.
+//  Copyright © 2020 erin. All rights reserved.
+//
+
+import XCTest
+@testable import videoForDancer
+
+class videoForDancerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
